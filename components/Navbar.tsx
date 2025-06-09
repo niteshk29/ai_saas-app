0a5a5627a474fc73b692f94c48e7c1d15f41fd1d
niteshk29 <niteshkhalia21.com>
2025-06-09 20:51:05 +0530

    added almost everything except bookmark

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
-import NavItems from "./NavItems";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import NavItems from "@/components/NavItems";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -11,7 +13,14 @@ const Navbar = () => {
       </Link>
       <div className="flex items-center gap-8">
         <NavItems />
-        <p>Sign In</p>
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
